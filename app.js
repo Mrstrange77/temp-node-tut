3ff4b9ee38622016d11179c417dac183c94a738f
Mrstrange77 <135584916+Mrstrange77@users.noreply.github.com>
2026-02-07 20:07:16 +0530

    first save

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const _=require('lodash')
+/*const _=require('lodash')
 
 const items=[1,[2,[3,[4]]]]
 const newItems=_.flattenDeep(items);
 console.log(newItems);
+console.log("hello vedant")*/
+
+//events 
+/*const EventEmitter = require('events')
+
+const  customEmitter=new EventEmitter();
+
+customEmitter.on('response',()=>{
+  console.log(`data received`)
+})
+customEmitter.on('response',()=>{
+  console.log(`some other logic here`)
+})
+customEmitter.emit('response')
+*/
